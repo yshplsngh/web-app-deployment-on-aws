4c5352abfd0072470eb01ae04d1730ab4811787b
yashpal singh <yashpalsingh9rx@outlook.com>
2024-01-03 16:53:35 +0530

    change index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/',(req,res)=>{
-    res.status(200).send("iam backend");
+    res.status(200).send("iam backend after changing");
 })
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
